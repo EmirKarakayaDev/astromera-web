ee9333771a824ac0dabd327d38e616f105f58225
Emir Karakaya <info@astromera.com>
2026-05-21 13:02:48 +0300

    Add Sanity Studio deployment config

---
+++ sanity.cli.js
@@ -0,0 +1,11 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'hkxp5ggh',
+    dataset: 'production',
+  },
+  deployment: {
+    appId: 'w1fn4eo2y8b062gvaagipvf7',
+  },
+})
